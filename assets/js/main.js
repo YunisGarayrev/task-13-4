3bac84caf82b7f0b475bc6e4b60b9a3bf9cff1db
YunisGarayev <79d2mw0@code.edu.az>
2023-04-13 13:07:37 -0700

    task 13-4 added

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+// Task1
+
+// var n,m,sum=0;
+// var n=prompt("reqemi daxil et");
+// while(n>0)
+// {
+//     m=n%10;    
+//     sum+=m;      
+//     n=(n-m)/10;   
+// }
+// alert(sum);
+
+
+
+// Task2
+
+
+var n=prompt("reqemi daxil et");
+while(n>1)
+{
+    if(n%2==0 || n%3==0 || n%5==0 ||n%7==0){
+    alert("murekkeb ededdir");
+}
+else{
+    alert("sade ededdir");
+}
+break;
+}
+
+
+
+
